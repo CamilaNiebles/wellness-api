624d3531e898dd9b3a3c985f97cdcb205da55ce0
CNieblesR <camila.niebles@rootstrap.com>
2023-08-17 21:38:22 -0500

    initial configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configModuleOptions } from '@config/config.module.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(configModuleOptions)],
+  imports: [
+    ConfigModule.forRoot(configModuleOptions),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
