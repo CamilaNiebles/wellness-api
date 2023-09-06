a5f6248a2778096f8ea18cc2818afe8a581e4dc5
CNieblesR <camila.niebles@rootstrap.com>
2023-09-05 22:14:26 -0500

    Create user module

--- src/models/repositories/user.repository.ts
+++ src/models/repositories/user.repository.ts
@@ -1,4 +1,6 @@
 import { BaseRepository } from '@abstractions/base.repository';
 import { User } from '@entities/users.entity';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class UserRepository extends BaseRepository<User> {}
